1de102dd863bf8703894edd77504061821d07db1
daulat <daulatjajra2006@gmail.com>
2024-02-06 09:49:08 +0530

    ex-im

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-"use strict";
+// Importing module
+// import { addToCart, totalPrice as price, qt } from "./shoppingCart.js";
+
+// addToCart("Private Jets", 5);
+// console.log(price, qt);
+
+// console.log("Importing module");
+
+// import * as ShoppingCart from "./shoppingCart.js";
+// ShoppingCart.addToCart("Bugati", 10);
+// console.log(ShoppingCart.totalPrice);
+// console.log(ShoppingCart.qt);
+
+// import add, { cart } from "./shoppingCart.js";
+// add("9zPC", 10);
+
+// console.log(cart);
+
+///////////////////////////////////////////////////////////////////////////////
+
+//// Top-leverl Await (ES 2022)
---
+++ shoppingCart.js
@@ -0,0 +1,20 @@
+// // Exporting module
+// console.log("Exporting module");
+
+// let shoppingCost = 10;
+// export let cart = [];
+
+// export let addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to the cart `);
+// };
+
+// let totalPrice = 2342;
+// let totalQunatity = 23;
+
+// export { totalPrice, totalQunatity as qt };
+
+// export default function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to the cart `);
+// }
