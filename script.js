c2b73bf20c59d8dd520653daf179c2ba4db3a223
daulat <daulatjajra2006@gmail.com>
2024-02-25 15:22:21 +0530

    repeat

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 // 1. firstly the exporting log will console then the importing log will console
 
 // Importing Named export module
-import { addToCart, totalPrice as price, qt } from "./shoppingCart.js";
+// import { addToCart, totalPrice as price, qt } from "./shoppingCart.js";
 
-addToCart("Private Jets", 5);
-// importing the changed value from default export
-console.log(price, qt);
-console.log("Importing module");
+// addToCart("Private Jets", 5);
+// // importing the changed value from default export
+// console.log(price, qt);
+// console.log("Importing module");
 
-// Importing the changed value from default export (differently)
-import * as ShoppingCart from "./shoppingCart.js";
-ShoppingCart.addToCart("Bugati", 10);
-// console.log(ShoppingCart.totalPrice);
-// console.log(ShoppingCart.qt);
+// // Importing the changed value from default export (differently)
+// import * as ShoppingCart from "./shoppingCart.js";
+// ShoppingCart.addToCart("Bugati", 10);
+// // console.log(ShoppingCart.totalPrice);
+// // console.log(ShoppingCart.qt);
 
-// Importing default export
-import add, { cart } from "./shoppingCart.js";
-add("9zPC", 10);
+// // Importing default export
+// import add, { cart } from "./shoppingCart.js";
+// add("9zPC", 10);
 // console.log(cart);
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -31,15 +67,15 @@ add("9zPC", 10);
 // console.log(data);
 // console.log("It's something after the fetch data");
 
-let getLastPost = async function () {
-  let res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  let data = await res.json();
-  console.log(data);
+// let getLastPost = async function () {
+//   let res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   let data = await res.json();
+//   console.log(data);
 
-  return { title: data.at(-1).title, body: data.at(-1).body };
-};
-let lastPost = getLastPost();
-console.log(lastPost); // Promise { <pending> }
+//   return { title: data.at(-1).title, body: data.at(-1).body };
+// };
+// let lastPost = getLastPost();
+// console.log(lastPost); // Promise { <pending> }
 
 // Not very clean
 // getLastPost().then((last) => console.log(last));
@@ -48,35 +84,35 @@ console.log(lastPost); // Promise { <pending> }
 
 //// The Module Pattern
 
-let shoppingCart2 = (function () {
-  let cart = [];
-  let shippingCost = 10;
-  let totalPrice = 234;
-  let totalQunatity = 23;
+// let shoppingCart2 = (function () {
+//   let cart = [];
+//   let shippingCost = 10;
+//   let totalPrice = 234;
+//   let totalQunatity = 23;
 
-  let addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${quantity} ${product} added to the cart (Shipping cost is ${shippingCost})`
-    );
-  };
-  let orderStack = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier `);
-  };
+//   let addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to the cart (Shipping cost is ${shippingCost})`
+//     );
+//   };
+//   let orderStack = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier `);
+//   };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQunatity,
-  };
-})();
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQunatity,
+//   };
+// })();
 
-shoppingCart2.addToCart("iphones", 3); //  3 iphones added to the cart (Shipping cost is 10)
-shoppingCart2.addToCart("samsung", 2); //  2 samsung added to the cart (Shipping cost is 10)
-console.log(shoppingCart2); // { cart: [ { product: 'iphones', quantity: 3 }, { product: 'samsung', quantity: 2 } ], totalPrice: 234, totalQunatity: 23 }
-console.log(shoppingCart2.cart);
-console.log(shoppingCart2.shippingCost); // undefined
+// shoppingCart2.addToCart("iphones", 3); //  3 iphones added to the cart (Shipping cost is 10)
+// shoppingCart2.addToCart("samsung", 2); //  2 samsung added to the cart (Shipping cost is 10)
+// console.log(shoppingCart2); // { cart: [ { product: 'iphones', quantity: 3 }, { product: 'samsung', quantity: 2 } ], totalPrice: 234, totalQunatity: 23 }
+// console.log(shoppingCart2.cart);
+// console.log(shoppingCart2.shippingCost); // undefined
 
 //////////////////////////////////////////////////////////////////////////////////////
 
@@ -100,47 +136,47 @@ console.log(shoppingCart2.shippingCost); // undefined
 // ///////////////////////////////////////////////////////////////////////
 
 ///// NPM :
-import cloneDeep from "lodash-es";
+// import cloneDeep from "lodash-es";
 
-let state = {
-  cart: [
-    { product: "bread", quantity: 5 },
-    { product: "pizza", quantity: 8 },
-  ],
-  user: { loggedIn: true },
-};
-// let stateClone = Object.assign({}, state);
+// let state = {
+//   cart: [
+//     { product: "bread", quantity: 5 },
+//     { product: "pizza", quantity: 8 },
+//   ],
+//   user: { loggedIn: true },
+// };
+// // let stateClone = Object.assign({}, state);
+// // state.user.loggedIn = false;
+// // console.log(stateClone);
+// let stateClone = cloneDeep(state);
 // state.user.loggedIn = false;
 // console.log(stateClone);
-let stateClone = cloneDeep(state);
-state.user.loggedIn = false;
-console.log(stateClone);
 
-//// Hot module : It'll help to not reload the page (only in parcel)
-if (module.hot) {
-  module.hot.accept();
-}
+// //// Hot module : It'll help to not reload the page (only in parcel)
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
-// Let's see how the ES6 class work in ES5 (Babel)
-class Person {
-  #greeting = "Hello";
-  constructor(name) {
-    this.name = name;
-    console.log(`${this.#greeting}, ${this.name}`);
-  }
-}
-let daulat = new Person("Daulat");
-console.log("Daulat" ?? null);
+// // Let's see how the ES6 class work in ES5 (Babel)
+// class Person {
+//   #greeting = "Hello";
+//   constructor(name) {
+//     this.name = name;
+//     console.log(`${this.#greeting}, ${this.name}`);
+//   }
+// }
+// let daulat = new Person("Daulat");
+// console.log("Daulat" ?? null);
 
-// Like these new features in ES6 can't be used in ES5 thatswhy we have to use polyfill
-console.log(cart.find((el) => el.quantity >= 2));
-Promise.resolve("This is resolved Promise").then((data) => console.log(data));
+// // Like these new features in ES6 can't be used in ES5 thatswhy we have to use polyfill
+// console.log(cart.find((el) => el.quantity >= 2));
+// Promise.resolve("This is resolved Promise").then((data) => console.log(data));
 
-import "core-js"; // It will polyfill the all features
+// import "core-js"; // It will polyfill the all features
 
-// for specific
-// import "core-js/stable/array/find";
-// import "core-js/stable/Promise";
+// // for specific
+// // import "core-js/stable/array/find";
+// // import "core-js/stable/Promise";
 
-//// polyfill for async functions
-import "regenerator-runtime/runtime.js";
+// //// polyfill for async functions
+// import "regenerator-runtime/runtime.js";
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,28 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// .
+// .
+// .
+// .
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Exporting module
-console.log("Exporting module");
+// console.log("Exporting module");
 
-// blocking code
-// console.log("Start fetching ");
-// await fetch(`https://jsonplaceholder.typicode.com/posts`);
-// console.log("It's something after the fetch data");
+// // blocking code
+// // console.log("Start fetching ");
+// // await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// // console.log("It's something after the fetch data");
 
-let shoppingCost = 10;
-export let cart = [];
+// let shoppingCost = 10;
+// export let cart = [];
 
-//// Named export (Always in top leverl of the module)
-export let addToCart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to the cart `);
-};
+// //// Named export (Always in top leverl of the module)
+// export let addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to the cart `);
+// };
 
-let totalPrice = 2342;
-let totalQunatity = 23;
+// let totalPrice = 2342;
+// let totalQunatity = 23;
 
-// change the default export
-export { totalPrice, totalQunatity as qt };
+// // change the default export
+// export { totalPrice, totalQunatity as qt };
 
-//// Default export
-export default function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to the cart `);
-}
+// //// Default export
+// export default function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to the cart `);
+// }
